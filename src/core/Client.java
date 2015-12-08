50e3e72d59886c6bc66c223c9de635531d70bfe2
Cristian Montejano <cristian_m15@yahoo.com>
2015-12-08 05:05:36 -0600

    Working build
    multiplayer snake game

---
+++ src/core/Client.java
@@ -0,0 +1,22 @@
+package core;
+public class Client
+{
+	public static void main (String [] args)
+	{
+		try
+		{
+			//makes a new game board
+			Board gameBoard = new Board();
+			
+			//adds a new game to the game board
+			gameBoard.add(new Game());	
+				
+			//sets the gameboard visible
+			gameBoard.setVisible(true);	
+		}
+		catch(Exception e)
+		{
+			System.out.println("Caught");
+		}	
+	}
+}
